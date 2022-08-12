8af6c2b7e2e8f8f3778b1a9025d88312827d3c27
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-08-11 22:04:55 -0600

    merge conflict start

--- x/cda/keeper/grpc_query_cda.go
+++ x/cda/keeper/grpc_query_cda.go
@@ -4,20 +4,29 @@ import (
 	"context"
 
 	"archive/x/cda/types"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"blog/x/blog/types"
 )
 
 func (k Keeper) Cda(goCtx context.Context, req *types.QueryCdaRequest) (*types.QueryCdaResponse, error) {
+	// Throw an error if the request is nil
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	// Unwrap context
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	// Load the CDA store
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(types.CDAKey))
+
+	//
+	store.Get([]byte(req.Id))
 
 	return &types.QueryCdaResponse{}, nil
 }
