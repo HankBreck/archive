2f6c32844fcd96695b860fb3de94600a458e0470
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-09-01 16:34:50 -0600

    Convert QueryCdas tests to new testing paradigm

--- x/cda/keeper/grpc_query_cdas_test.go
+++ x/cda/keeper/grpc_query_cdas_test.go
@@ -1,46 +1,123 @@
 package keeper_test
 
 import (
-	testkeeper "archive/testutil/keeper"
 	"archive/x/cda/types"
+	goctx "context"
 	"encoding/binary"
-	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
-	"github.com/stretchr/testify/require"
 )
 
-func TestCdasQuery(t *testing.T) {
-	keeper, ctx := testkeeper.CdaKeeper(t)
+func (suite *KeeperTestSuite) TestQueryCdas() {
+	queryClient := suite.queryClient
+	owner := suite.TestAccs[0]
+	ids := suite.PrepareCdasForOwner(owner, 5)
 
-	testkeeper.PopulateCdas(CREATOR, 5, keeper, ctx)
+	response, err := queryClient.Cdas(goctx.Background(), &types.QueryCdasRequest{})
 
-	// Success on empty request
-	req := types.QueryCdasRequest{}
-	res, err := keeper.Cdas(sdk.WrapSDKContext(ctx), &req)
-	require.Nil(t, err)
-	require.Equal(t, len(res.CDAs), 5)
-	require.Equal(t, res.CDAs[0].Id, uint64(0))
-	require.Nil(t, res.Pagination.NextKey)
+	suite.Require().NoError(err)
+	suite.Require().Nil(response.Pagination.NextKey)
+	suite.Require().EqualValues(5, len(response.CDAs))
 
-	// Reset res and err
-	res = nil
-	err = nil
-
-	// Create the byte key for querying CDAs with ids from 2 to 4
-	byteId := make([]byte, 8)
-	binary.BigEndian.PutUint64(byteId, 2)
-
-	// Success viewing CDAs with ids from 2 to 4
-	req = types.QueryCdasRequest{
-		Pagination: &query.PageRequest{
-			Key: byteId,
-		},
+	for i := 0; i < len(response.CDAs); i++ {
+		suite.Require().Contains(ids, response.CDAs[i].Id)
+		suite.Require().EqualValues(owner.String(), response.CDAs[i].Creator)
 	}
-	res, err = keeper.Cdas(sdk.WrapSDKContext(ctx), &req)
-	require.Nil(t, err)
-	require.Equal(t, len(res.CDAs), 3)
-	require.Equal(t, res.CDAs[0].Id, uint64(2))
-	require.Equal(t, res.CDAs[2].Id, uint64(4))
+}
+
+func (suite *KeeperTestSuite) TestQueryCdas_Paginate() {
+	queryClient := suite.queryClient
+	firstOwner := suite.TestAccs[0]
+	secondOwner := suite.TestAccs[1]
+	firstIds := suite.PrepareCdasForOwner(firstOwner, 5)
+	secondIds := suite.PrepareCdasForOwner(secondOwner, 5)
+
+	// Get the first 5 elements, starting with 0
+	bzKey := make([]byte, 8)
+	binary.BigEndian.PutUint64(bzKey, 0)
+	pagination := &query.PageRequest{
+		Key:   bzKey,
+		Limit: 5,
+	}
+	response, err := queryClient.Cdas(goctx.Background(), &types.QueryCdasRequest{Pagination: pagination})
+
+	// firstIds references the first 5 elements in storage
+	suite.Require().NoError(err)
+	suite.Require().Len(response.CDAs, 5)
+	for i := 0; i < 5; i++ {
+		suite.Require().Equal(firstIds[i], response.CDAs[i].Id)
+	}
+
+	// Fetch the next five using response.Pagination.NextKey
+	finalPagination := &query.PageRequest{
+		Key:        response.Pagination.NextKey,
+		Limit:      5,
+		CountTotal: true,
+	}
+	finalResponse, finalErr := queryClient.Cdas(goctx.Background(), &types.QueryCdasRequest{Pagination: finalPagination})
+
+	suite.Require().NoError(finalErr)
+	suite.Require().Len(finalResponse.CDAs, 5)
+	for i := 0; i < 5; i++ {
+		suite.Require().Equal(secondIds[i], finalResponse.CDAs[i].Id)
+		suite.Require().Equal(secondOwner.String(), finalResponse.CDAs[i].Creator)
+	}
+}
+
+func (suite *KeeperTestSuite) TestQueryCdas_PaginateReversed() {
+	queryClient := suite.queryClient
+	owner := suite.TestAccs[0]
+	ids := suite.PrepareCdasForOwner(owner, 10)
+
+	// Get the last 5 elements stored
+	pagination := &query.PageRequest{
+		// For some reason we can't pass the max key...
+		Limit:      5,
+		Reverse:    true,
+		CountTotal: true,
+	}
+	response, err := queryClient.Cdas(goctx.Background(), &types.QueryCdasRequest{Pagination: pagination})
+
+	suite.Require().EqualValues(uint64(10), response.Pagination.Total)
+	suite.Require().NoError(err)
+	suite.Require().Len(response.CDAs, 5)
+	for i := 0; i < 5; i++ {
+		// Order is reversed so need to index ids with len(ids)-i
+		suite.Require().Equal(ids[9-i], response.CDAs[i].Id)
+		suite.Require().Equal(owner.String(), response.CDAs[i].Creator)
+	}
+
+	// Fetch the next five using response.Pagination.NextKey
+	finalPagination := &query.PageRequest{
+		Key:        response.Pagination.NextKey,
+		Limit:      5,
+		Reverse:    true,
+		CountTotal: true,
+	}
+	finalResponse, finalErr := queryClient.Cdas(goctx.Background(), &types.QueryCdasRequest{Pagination: finalPagination})
+
+	suite.Require().NoError(finalErr)
+	suite.Require().Len(finalResponse.CDAs, 5)
+	for i := 0; i < 5; i++ {
+		// Order is reversed and this is the second batch,
+		// so need to index ids with len(ids)-5-i
+		suite.Require().Equal(ids[4-i], finalResponse.CDAs[i].Id)
+		suite.Require().Equal(owner.String(), finalResponse.CDAs[i].Creator)
+	}
+}
+
+func (suite *KeeperTestSuite) TestQueryCdas_InvalidKey() {
+	queryClient := suite.queryClient
+	owner := suite.TestAccs[0]
+	suite.PrepareCdasForOwner(owner, 5)
+
+	invalidKey := make([]byte, 8)
+	binary.BigEndian.PutUint64(invalidKey, uint64(6))
+	pagination := &query.PageRequest{
+		Key: invalidKey,
+	}
+	response, err := queryClient.Cdas(goctx.Background(), &types.QueryCdasRequest{Pagination: pagination})
+
+	suite.Require().NoError(err)
+	suite.Require().Len(response.CDAs, 0)
 }
