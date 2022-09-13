1f6951f31db20f133a10539e6764f8a1a1309a5a
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-09-13 15:03:05 -0600

    Generate ts-client using ignite

---
+++ vue/src/generated/archive.cda/registry.ts
@@ -0,0 +1,9 @@
+import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgCreateCDA } from "./types/cda/tx";
+
+const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/archive.cda.MsgCreateCDA", MsgCreateCDA],
+    
+];
+
+export { msgTypes }
---
+++ vue/src/generated/archive.cda/types.ts
@@ -0,0 +1,11 @@
+import { CDA } from "./types/cda/cda"
+import { Ownership } from "./types/cda/cda"
+import { Params } from "./types/cda/params"
+
+
+export {     
+    CDA,
+    Ownership,
+    Params,
+    
+ }
