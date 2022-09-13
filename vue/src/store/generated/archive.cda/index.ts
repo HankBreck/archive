0f4a3222b1e9d63f24165bf4886e072e87d13684
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-09-13 15:03:30 -0600

    Generate vuex and ts-client using ignite

---
+++ vue/src/store/generated/archive.cda/index.ts
@@ -0,0 +1,259 @@
+import { Client, registry, MissingWalletError } from 'archive-client-ts'
+
+import { CDA } from "archive-client-ts/archive.cda/types"
+import { Ownership } from "archive-client-ts/archive.cda/types"
+import { Params } from "archive-client-ts/archive.cda/types"
+
+
+export { CDA, Ownership, Params };
+
+function initClient(vuexGetters) {
+	return new Client(vuexGetters['common/env/getEnv'], vuexGetters['common/wallet/signer'])
+}
+
+function mergeResults(value, next_values) {
+	for (let prop of Object.keys(next_values)) {
+		if (Array.isArray(next_values[prop])) {
+			value[prop]=[...value[prop], ...next_values[prop]]
+		}else{
+			value[prop]=next_values[prop]
+		}
+	}
+	return value
+}
+
+type Field = {
+	name: string;
+	type: unknown;
+}
+function getStructure(template) {
+	let structure: {fields: Field[]} = { fields: [] }
+	for (const [key, value] of Object.entries(template)) {
+		let field = { name: key, type: typeof value }
+		structure.fields.push(field)
+	}
+	return structure
+}
+const getDefaultState = () => {
+	return {
+				Params: {},
+				Cda: {},
+				Cdas: {},
+				CdasOwned: {},
+				
+				_Structure: {
+						CDA: getStructure(CDA.fromPartial({})),
+						Ownership: getStructure(Ownership.fromPartial({})),
+						Params: getStructure(Params.fromPartial({})),
+						
+		},
+		_Registry: registry,
+		_Subscriptions: new Set(),
+	}
+}
+
+// initial state
+const state = getDefaultState()
+
+export default {
+	namespaced: true,
+	state,
+	mutations: {
+		RESET_STATE(state) {
+			Object.assign(state, getDefaultState())
+		},
+		QUERY(state, { query, key, value }) {
+			state[query][JSON.stringify(key)] = value
+		},
+		SUBSCRIBE(state, subscription) {
+			state._Subscriptions.add(JSON.stringify(subscription))
+		},
+		UNSUBSCRIBE(state, subscription) {
+			state._Subscriptions.delete(JSON.stringify(subscription))
+		}
+	},
+	getters: {
+				getParams: (state) => (params = { params: {}}) => {
+					if (!(<any> params).query) {
+						(<any> params).query=null
+					}
+			return state.Params[JSON.stringify(params)] ?? {}
+		},
+				getCda: (state) => (params = { params: {}}) => {
+					if (!(<any> params).query) {
+						(<any> params).query=null
+					}
+			return state.Cda[JSON.stringify(params)] ?? {}
+		},
+				getCdas: (state) => (params = { params: {}}) => {
+					if (!(<any> params).query) {
+						(<any> params).query=null
+					}
+			return state.Cdas[JSON.stringify(params)] ?? {}
+		},
+				getCdasOwned: (state) => (params = { params: {}}) => {
+					if (!(<any> params).query) {
+						(<any> params).query=null
+					}
+			return state.CdasOwned[JSON.stringify(params)] ?? {}
+		},
+				
+		getTypeStructure: (state) => (type) => {
+			return state._Structure[type].fields
+		},
+		getRegistry: (state) => {
+			return state._Registry
+		}
+	},
+	actions: {
+		init({ dispatch, rootGetters }) {
+			console.log('Vuex module: archive.cda initialized!')
+			if (rootGetters['common/env/client']) {
+				rootGetters['common/env/client'].on('newblock', () => {
+					dispatch('StoreUpdate')
+				})
+			}
+		},
+		resetState({ commit }) {
+			commit('RESET_STATE')
+		},
+		unsubscribe({ commit }, subscription) {
+			commit('UNSUBSCRIBE', subscription)
+		},
+		async StoreUpdate({ state, dispatch }) {
+			state._Subscriptions.forEach(async (subscription) => {
+				try {
+					const sub=JSON.parse(subscription)
+					await dispatch(sub.action, sub.payload)
+				}catch(e) {
+					throw new Error('Subscriptions: ' + e.message)
+				}
+			})
+		},
+		
+		
+		
+		 		
+		
+		
+		async QueryParams({ commit, rootGetters, getters }, { options: { subscribe, all} = { subscribe:false, all:false}, params, query=null }) {
+			try {
+				const key = params ?? {};
+				const client = initClient(rootGetters);
+				let value= (await client.ArchiveCda.query.queryParams()).data
+				
+					
+				commit('QUERY', { query: 'Params', key: { params: {...key}, query}, value })
+				if (subscribe) commit('SUBSCRIBE', { action: 'QueryParams', payload: { options: { all }, params: {...key},query }})
+				return getters['getParams']( { params: {...key}, query}) ?? {}
+			} catch (e) {
+				throw new Error('QueryClient:QueryParams API Node Unavailable. Could not perform query: ' + e.message)
+				
+			}
+		},
+		
+		
+		
+		
+		 		
+		
+		
+		async QueryCda({ commit, rootGetters, getters }, { options: { subscribe, all} = { subscribe:false, all:false}, params, query=null }) {
+			try {
+				const key = params ?? {};
+				const client = initClient(rootGetters);
+				let value= (await client.ArchiveCda.query.queryCda( key.id)).data
+				
+					
+				commit('QUERY', { query: 'Cda', key: { params: {...key}, query}, value })
+				if (subscribe) commit('SUBSCRIBE', { action: 'QueryCda', payload: { options: { all }, params: {...key},query }})
+				return getters['getCda']( { params: {...key}, query}) ?? {}
+			} catch (e) {
+				throw new Error('QueryClient:QueryCda API Node Unavailable. Could not perform query: ' + e.message)
+				
+			}
+		},
+		
+		
+		
+		
+		 		
+		
+		
+		async QueryCdas({ commit, rootGetters, getters }, { options: { subscribe, all} = { subscribe:false, all:false}, params, query=null }) {
+			try {
+				const key = params ?? {};
+				const client = initClient(rootGetters);
+				let value= (await client.ArchiveCda.query.queryCdas(query ?? undefined)).data
+				
+					
+				while (all && (<any> value).pagination && (<any> value).pagination.next_key!=null) {
+					let next_values=(await client.ArchiveCda.query.queryCdas({...query ?? {}, 'pagination.key':(<any> value).pagination.next_key} as any)).data
+					value = mergeResults(value, next_values);
+				}
+				commit('QUERY', { query: 'Cdas', key: { params: {...key}, query}, value })
+				if (subscribe) commit('SUBSCRIBE', { action: 'QueryCdas', payload: { options: { all }, params: {...key},query }})
+				return getters['getCdas']( { params: {...key}, query}) ?? {}
+			} catch (e) {
+				throw new Error('QueryClient:QueryCdas API Node Unavailable. Could not perform query: ' + e.message)
+				
+			}
+		},
+		
+		
+		
+		
+		 		
+		
+		
+		async QueryCdasOwned({ commit, rootGetters, getters }, { options: { subscribe, all} = { subscribe:false, all:false}, params, query=null }) {
+			try {
+				const key = params ?? {};
+				const client = initClient(rootGetters);
+				let value= (await client.ArchiveCda.query.queryCdasOwned( key.owner, query ?? undefined)).data
+				
+					
+				while (all && (<any> value).pagination && (<any> value).pagination.next_key!=null) {
+					let next_values=(await client.ArchiveCda.query.queryCdasOwned( key.owner, {...query ?? {}, 'pagination.key':(<any> value).pagination.next_key} as any)).data
+					value = mergeResults(value, next_values);
+				}
+				commit('QUERY', { query: 'CdasOwned', key: { params: {...key}, query}, value })
+				if (subscribe) commit('SUBSCRIBE', { action: 'QueryCdasOwned', payload: { options: { all }, params: {...key},query }})
+				return getters['getCdasOwned']( { params: {...key}, query}) ?? {}
+			} catch (e) {
+				throw new Error('QueryClient:QueryCdasOwned API Node Unavailable. Could not perform query: ' + e.message)
+				
+			}
+		},
+		
+		
+		async sendMsgCreateCDA({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const client=await initClient(rootGetters)
+				const result = await client.ArchiveCda.tx.sendMsgCreateCDA({ value, fee: {amount: fee, gas: "200000"}, memo })
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgCreateCDA:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgCreateCDA:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
+		
+		async MsgCreateCDA({ rootGetters }, { value }) {
+			try {
+				const client=initClient(rootGetters)
+				const msg = await client.ArchiveCda.tx.msgCreateCDA({value})
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgCreateCDA:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgCreateCDA:Create Could not create message: ' + e.message)
+				}
+			}
+		},
+		
+	}
+}
