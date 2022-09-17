d81986c213eb58c74db5ea7c8b8567e568237b58
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-09-17 11:31:38 -0600

    Add tests for MsgFinalizeCda

--- x/cda/keeper/finalize_test.go
+++ x/cda/keeper/finalize_test.go
@@ -1,14 +1,27 @@
 package keeper_test
 
 import (
+	"archive/x/cda/keeper"
 	"archive/x/cda/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// Assert that it fails on nonexisting CdaId
-// Assert that it fails when missing approvals
-// Assert that it fails when already approved
+func approveAll(k keeper.Keeper, ctx sdk.Context, cda *types.CDA, owners []*sdk.AccAddress) {
+	// Send approval for each owner
+	ownerships := (*cda).Ownership
+	for _, owner := range owners {
+		msg := types.MsgApproveCda{
+			Creator:   owner.String(),
+			CdaId:     cda.Id,
+			Ownership: ownerships,
+		}
+		err := k.SetApproval(ctx, &msg)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
 // Assert it works as normal
 func (suite *KeeperTestSuite) TestFinalizeCda() {
@@ -16,20 +29,9 @@ func (suite *KeeperTestSuite) TestFinalizeCda() {
 	owners := []*sdk.AccAddress{&suite.TestAccs[0], &suite.TestAccs[1]}
 	ids := suite.PrepareCdasForOwner(owners, 1)
 	cdas := suite.GetCdas(ids)
-
 	cda := cdas[0]
 
-	// Send approval for each owner
-	ownerships := (*cda).Ownership
-	for _, owner := range owners {
-		msg := types.MsgApproveCda{
-			Creator:   owner.String(),
-			CdaId:     ids[0],
-			Ownership: ownerships,
-		}
-		err := k.SetApproval(suite.Ctx, &msg)
-		suite.NoError(err)
-	}
+	approveAll(k, suite.Ctx, cda, owners)
 
 	// Send finalize message
 	msg := types.MsgFinalizeCda{
@@ -45,4 +47,83 @@ func (suite *KeeperTestSuite) TestFinalizeCda() {
 	res, err := suite.queryClient.Cda(suite.Ctx.Context(), &queryMsg)
 	suite.NoError(err)
 	suite.True(res.Cda.Approved)
+}
+
+// Assert that it fails on nonexisting CdaId
+func (suite *KeeperTestSuite) TestFinalizeCda__NonexistentCdaId() {
+	k := suite.App.CdaKeeper
+	owners := []*sdk.AccAddress{&suite.TestAccs[0], &suite.TestAccs[1]}
+	ids := suite.PrepareCdasForOwner(owners, 1)
+	cdas := suite.GetCdas(ids)
+	cda := cdas[0]
+
+	approveAll(k, suite.Ctx, cda, owners)
+
+	// Send finalize message with a nonexistent CdaId
+	msg := types.MsgFinalizeCda{
+		Creator: owners[0].String(),
+		CdaId:   cda.Id + 1,
+	}
+	err := k.FinalizeCda(suite.Ctx, &msg)
+	suite.EqualError(err, "Invalid CdaId. Please ensure the CDA exists for the given ID.")
+
+	// Ensure the real CDA was not finalized
+	queryMsg := types.QueryCdaRequest{
+		Id: cda.Id,
+	}
+	res, err := suite.queryClient.Cda(suite.Ctx.Context(), &queryMsg)
+	suite.NoError(err)
+	suite.False(res.Cda.Approved)
+}
+
+// Assert that it fails when missing approvals
+func (suite *KeeperTestSuite) TestFinalizeCda__MissingApproval() {
+	k := suite.App.CdaKeeper
+	owners := []*sdk.AccAddress{&suite.TestAccs[0], &suite.TestAccs[1]}
+	ids := suite.PrepareCdasForOwner(owners, 1)
+	cdas := suite.GetCdas(ids)
+	cda := cdas[0]
+
+	// Send approval from owners[0] but not owners[1]
+	approveAll(k, suite.Ctx, cda, owners[:1])
+
+	// Send finalize message with a nonexistent CdaId
+	msg := types.MsgFinalizeCda{
+		Creator: owners[0].String(),
+		CdaId:   cda.Id,
+	}
+	err := k.FinalizeCda(suite.Ctx, &msg)
+	expectedErr := types.ErrMissingApproval.Wrapf("The CDA with an ID of %d is missing approval from account %s", cda.Id, owners[1].String())
+	suite.EqualError(err, expectedErr.Error())
+
+	// Ensure the CDA was not finalized
+	queryMsg := types.QueryCdaRequest{
+		Id: cda.Id,
+	}
+	res, err := suite.queryClient.Cda(suite.Ctx.Context(), &queryMsg)
+	suite.NoError(err)
+	suite.False(res.Cda.Approved)
+}
+
+// Assert that it fails when already finalized
+func (suite *KeeperTestSuite) TestFinalizeCda_DoubleApproval() {
+	k := suite.App.CdaKeeper
+	owners := []*sdk.AccAddress{&suite.TestAccs[0], &suite.TestAccs[1]}
+	ids := suite.PrepareCdasForOwner(owners, 1)
+	cdas := suite.GetCdas(ids)
+	cda := cdas[0]
+
+	approveAll(k, suite.Ctx, cda, owners)
+
+	// Send finalize message
+	msg := types.MsgFinalizeCda{
+		Creator: owners[0].String(),
+		CdaId:   cda.Id,
+	}
+	err := k.FinalizeCda(suite.Ctx, &msg)
+	suite.NoError(err)
+
+	// Assert failure on second finalize message
+	err2 := k.FinalizeCda(suite.Ctx, &msg)
+	suite.EqualError(err2, "CDA has already been finalized.")
 }
