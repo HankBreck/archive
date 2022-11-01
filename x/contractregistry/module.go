16d067ef5cbee2aaa8f0f04702f5b878dffbcb37
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-11-01 11:56:22 -0600

    Add RegisterRESTRoutes method for contractregistry module's AppModuleBasic

--- x/contractregistry/module.go
+++ x/contractregistry/module.go
@@ -7,6 +7,7 @@ import (
 
 	// this line is used by starport scaffolding # 1
 
+	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
 
@@ -69,6 +70,9 @@ func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncod
 	}
 	return genState.Validate()
 }
+
+// RegisterRESTRoutes registers the capability module's REST service handlers.
+func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Router) {}
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
