bb01b3b99ccfdc907b6aff1694767500cced3434
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-11-02 13:06:40 -0600

    Outline the RegisterContract message in msgServer

--- x/contractregistry/keeper/msg_server.go
+++ x/contractregistry/keeper/msg_server.go
@@ -2,7 +2,12 @@ package keeper
 
 import (
 	"archive/x/contractregistry/types"
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
+
+var _ types.MsgServer = msgServer{}
 
 type msgServer struct {
 	Keeper
@@ -14,4 +19,17 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 	return &msgServer{Keeper: keeper}
 }
 
-var _ types.MsgServer = msgServer{}
+func (m msgServer) RegisterContract(goCtx context.Context, msg *types.MsgRegisterContract) (*types.MsgRegisterContractResponse, error) {
+
+	// Unwrap the context
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+
+	// Store the Contract in state
+
+	// Store the schema in state
+
+	// Emit Events
+
+	return &types.MsgRegisterContractResponse{Id: uint64(1)}, nil
+}
