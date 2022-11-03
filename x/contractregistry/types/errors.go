12233f1894f42f85d8234999f38e79523c0a1927
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-11-03 13:46:38 -0600

    Add new errors to contract module

--- x/contractregistry/types/errors.go
+++ x/contractregistry/types/errors.go
@@ -8,7 +8,8 @@ import (
 
 // x/contractregistry module sentinel errors
 var (
-	ErrEmpty               = sdkerrors.Register(ModuleName, 2, "empty")
-	ErrInvalid             = sdkerrors.Register(ModuleName, 3, "invalid")
-	ErrNonExistentContract = sdkerrors.Register(ModuleName, 4, "Invalid Contract ID. Please ensure the Contract exists for the given ID.")
+	ErrEmpty                  = sdkerrors.Register(ModuleName, 2, "empty")
+	ErrInvalid                = sdkerrors.Register(ModuleName, 3, "invalid")
+	ErrNonExistentContract    = sdkerrors.Register(ModuleName, 4, "Invalid Contract ID. Please ensure the Contract exists for the given ID.")
+	ErrNonExistentSigningData = sdkerrors.Register(ModuleName, 4, "Signing data was not found.")
 )
