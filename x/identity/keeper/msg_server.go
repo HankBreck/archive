e4c5f376d2867c173693674df9354093c45121c4
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-12-20 11:23:22 -0600

    WIP: Add RegisterIssuer method to identity keeper

--- x/identity/keeper/msg_server.go
+++ x/identity/keeper/msg_server.go
@@ -2,11 +2,16 @@ package keeper
 
 import (
 	"archive/x/identity/types"
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 type msgServer struct {
 	Keeper
 }
+
+var _ types.MsgServer = msgServer{}
 
 // NewMsgServerImpl returns an implementation of the MsgServer interface
 // for the provided Keeper.
@@ -14,4 +19,14 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 	return &msgServer{Keeper: keeper}
 }
 
-var _ types.MsgServer = msgServer{}
+func (k msgServer) RegisterIssuer(goCtx context.Context, msg *types.MsgRegisterIssuer) (*types.MsgRegisterIssuerResponse, error) {
+	if msg == nil {
+		return nil, types.ErrInvalid.Wrap("Type MsgRegisterContract cannot be nil.")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	_ = ctx
+
+	return &types.MsgRegisterIssuerResponse{}, nil
+}
