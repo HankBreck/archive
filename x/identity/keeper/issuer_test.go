6cb3eceb829ae2407ce6a3d08a4998cd843c0acf
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-12-27 10:37:33 -0600

    Add initial tests for issuer keeper methods

--- x/identity/keeper/issuer_test.go
+++ x/identity/keeper/issuer_test.go
@@ -1,11 +1,71 @@
 package keeper_test
 
-//TODO:
-//	test cases like in contract_test.go
-//		base case
-//		overwrite preset id
-//		valid address
-//	special cases
-//		sequential IDs
-//		get issuer
-//		has issuer
+import (
+	"archive/x/identity/types"
+)
+
+// TODO:
+//		Add test for GetIssuers (paged)
+
+func (suite *KeeperTestSuite) TestSetIssuer() {
+	k := suite.App.IdentityKeeper
+	issuer := types.Issuer{
+		Creator:     suite.TestAccs[0].String(),
+		Name:        "Test Issuer",
+		MoreInfoUri: "google.com/more-info",
+		Cost:        0,
+	}
+	err := k.SetIssuer(suite.Ctx, issuer)
+	suite.NoError(err)
+}
+
+func (suite *KeeperTestSuite) TestSetIssuer_DuplicateIssuer() {
+	k := suite.App.IdentityKeeper
+	issuer := types.Issuer{
+		Creator:     suite.TestAccs[0].String(),
+		Name:        "Test Issuer",
+		MoreInfoUri: "google.com/more-info",
+		Cost:        0,
+	}
+	// First time setting works
+	err := k.SetIssuer(suite.Ctx, issuer)
+	suite.NoError(err)
+
+	// Second time setting throws an error
+	err = k.SetIssuer(suite.Ctx, issuer)
+	suite.ErrorIs(err, types.ErrExistingIssuer)
+}
+
+func (suite *KeeperTestSuite) TestGetIssuer() {
+	// Prepare state
+	k := suite.App.IdentityKeeper
+	creator := suite.TestAccs[0].String()
+	expectedIssuer := types.Issuer{
+		Creator:     creator,
+		Name:        "Test Issuer",
+		MoreInfoUri: "google.com/more-info",
+		Cost:        0,
+	}
+	k.SetIssuer(suite.Ctx, expectedIssuer)
+
+	// Retrieve expected issuer object
+	actualIssuer, err := k.GetIssuer(suite.Ctx, creator)
+	suite.NoError(err)
+	suite.Equal(expectedIssuer, *actualIssuer)
+}
+
+func (suite *KeeperTestSuite) TestHasIssuer() {
+	// Prepare state
+	k := suite.App.IdentityKeeper
+	creator := suite.TestAccs[0].String()
+	expectedIssuer := types.Issuer{
+		Creator:     creator,
+		Name:        "Test Issuer",
+		MoreInfoUri: "google.com/more-info",
+		Cost:        0,
+	}
+	k.SetIssuer(suite.Ctx, expectedIssuer)
+
+	suite.True(k.HasIssuer(suite.Ctx, creator))
+	suite.False(k.HasIssuer(suite.Ctx, suite.TestAccs[1].String()))
+}
