49872fa83fcf37c57286697fb802a157966b53cc
Hank Breckenridge <breckenridgeh2@gmail.com>
2023-01-03 15:16:39 -0600

    Fix scaffolding formats

--- x/identity/keeper/grpc_query.go
+++ x/identity/keeper/grpc_query.go
@@ -54,3 +54,16 @@ func (k Keeper) Issuers(goCtx context.Context, req *types.QueryIssuersRequest) (
 		Pagination: pageRes,
 	}, nil
 }
+
+func (k Keeper) IssuerInfo(goCtx context.Context, req *types.QueryIssuerInfoRequest) (*types.QueryIssuerInfoResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Process the query
+	_ = ctx
+
+	return &types.QueryIssuerInfoResponse{}, nil
+}
