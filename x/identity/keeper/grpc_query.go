3b5ef35926de1599c61849b2fba2fadc1e0373cf
Hank Breckenridge <breckenridgeh2@gmail.com>
2023-01-05 12:40:10 -0600

    Clean up ignite scaffolding

--- x/identity/keeper/grpc_query.go
+++ x/identity/keeper/grpc_query.go
@@ -107,3 +107,16 @@ func (k Keeper) Operators(goCtx context.Context, req *types.QueryOperatorsReques
 		Pagination: pageRes,
 	}, nil
 }
+
+func (k Keeper) MemberRole(goCtx context.Context, req *types.QueryMemberRoleRequest) (*types.QueryMemberRoleResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Process the query
+	_ = ctx
+
+	return &types.QueryMemberRoleResponse{}, nil
+}
