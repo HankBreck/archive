a9c8e185c1a3fd5be554f87569e38149dff123e1
Hank Breckenridge <breckenridgeh2@gmail.com>
2023-01-14 18:45:50 -0700

    Successfully generate go code from protobuf

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -10,19 +10,7 @@ go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 
 echo "Generating gogo proto code"
 cd proto
-echo "1"
-proto_dirs=$(find ./archive -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-echo "2"
-# for dir in $proto_dirs; do
-#   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-#     if grep go_package $file &>/dev/null; then
-#       buf generate --template buf.gen.gogo.yaml $file
-#     fi
-#   done
-# done
-echo "Running cmd"
 buf generate --template buf.gen.gogo.yaml
-echo "3"
 
 cd ..
 
