68c1a322d7da29b97670e8b99828662b9c157d2d
Hank Breckenridge <breckenridgeh2@gmail.com>
2023-02-20 20:28:02 -0700

    remove vue from repo

--- vue/src/generated/archive.contractregistry/registry.ts
+++
@@ -1,9 +0,0 @@
-import { GeneratedType } from "@cosmjs/proto-signing";
-import { MsgRegisterContract } from "./types/archive/contractregistry/tx";
-
-const msgTypes: Array<[string, GeneratedType]>  = [
-    ["/archive.contractregistry.MsgRegisterContract", MsgRegisterContract],
-    
-];
-
-export { msgTypes }
