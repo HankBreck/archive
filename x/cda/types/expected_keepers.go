6a4c3a6e54e9345374b7a9aa033196917e706a09
Hank Breckenridge <breckenridgeh2@gmail.com>
2023-02-22 10:18:38 -0700

    Add IdentityKeeper to CDA expected keepers

--- x/cda/types/expected_keepers.go
+++ x/cda/types/expected_keepers.go
@@ -1,1 +1,10 @@
 package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type IdentityKeeper interface {
+	HasMember(ctx sdk.Context, certificateId uint64, member sdk.AccAddress) (bool, error)
+	HasCertificate(ctx sdk.Context, id uint64) bool
+}
