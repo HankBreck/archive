f007879e5e4bab312886494254ac5b693b31e76f
Hank Breckenridge <breckenridgeh2@gmail.com>
2023-03-05 12:09:41 -0700

    Add test for HasApproval

--- x/cda/keeper/approval_test.go
+++ x/cda/keeper/approval_test.go
@@ -27,3 +27,18 @@ func (suite *KeeperTestSuite) TestSetApproval_ApproveTwice() {
 	err = k.SetApproval(suite.Ctx, cdaIds[0], signerIds[0])
 	suite.EqualError(err, "The address has already given approval for this CDA.")
 }
+
+func (suite *KeeperTestSuite) TestHasApproval() {
+	signers := []*sdk.AccAddress{&suite.TestAccs[0]}
+	cdaIds, signerIds := suite.PrepareCdas(signers, 1)
+	k := suite.App.CdaKeeper
+
+	// Ensure false when approval has not been received
+	suite.False(k.HasApproval(suite.Ctx, cdaIds[0], signerIds[0]))
+
+	// Update state to set approval from signer
+	k.SetApproval(suite.Ctx, cdaIds[0], signerIds[0])
+
+	// Ensure true after update
+	suite.True(k.HasApproval(suite.Ctx, cdaIds[0], signerIds[0]))
+}
