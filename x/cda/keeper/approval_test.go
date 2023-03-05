503317b22c9e9d090edfe29360e1650330d7744d
Hank Breckenridge <breckenridgeh2@gmail.com>
2023-03-05 12:01:23 -0700

    Clean up outdated approval tests

--- x/cda/keeper/approval_test.go
+++ x/cda/keeper/approval_test.go
@@ -1,8 +1,6 @@
 package keeper_test
 
 import (
-	"github.com/HankBreck/archive/x/cda/types"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -38,56 +36,4 @@ func (suite *KeeperTestSuite) TestSetApproval_NonexistentCdaId() {
 
 	err := k.SetApproval(suite.Ctx, cdaIds[0], signerIds[0])
 	suite.EqualError(err, "Invalid CdaId. Please ensure the CDA exists for the given ID.")
-}
-
-// Assert fails with error on invalid signing data
-func (suite *KeeperTestSuite) TestSetApproval_InvalidSigningData() {
-	signers := []*sdk.AccAddress{&suite.TestAccs[0]}
-	cdaIds, signerIds := suite.PrepareCdas(signers, 1)
-	k := suite.App.CdaKeeper
-
-	var invalidSigningData types.RawSigningData
-	invalidSigningData.UnmarshalJSON([]byte(`
-	{
-		"notOwnerships": [
-			{ "owner": "address", "ownership_proportion": 1 },
-			{ "owner": "address2", "ownership_proportion": 99 }
-		]
-	}`))
-	msg := types.MsgApproveCda{
-		Creator:     signers[0].String(),
-		CdaId:       ids[0],
-		SigningData: invalidSigningData,
-	}
-	err := k.SetApproval(suite.Ctx, &msg)
-	suite.EqualError(err, "Signing data provided does not match the signing data stored in the CDA.")
-}
-
-// Assert fails with error on non-owner Creator
-func (suite *KeeperTestSuite) TestSetApproval_UnauthorizedCreator() {
-	signers := []*sdk.AccAddress{&suite.TestAccs[0]}
-	ids := suite.PrepareCdasForOwner(signers, 1)
-	k := suite.App.CdaKeeper
-	msg := types.MsgApproveCda{
-		Creator:     suite.TestAccs[1].String(),
-		CdaId:       ids[0],
-		SigningData: suite.GetSigningData(),
-	}
-	err := k.SetApproval(suite.Ctx, &msg)
-	suite.EqualError(err, "Signer is not an owner of cda 0: unauthorized")
-}
-
-// Assert fails with error on invalid cda.Status
-func (suite *KeeperTestSuite) TestSetApproval_WrongStatus() {
-	signers := []*sdk.AccAddress{&suite.TestAccs[0]}
-	id := suite.PrepareVoidedCdaForSigners(signers)
-	k := suite.App.CdaKeeper
-
-	msg := types.MsgApproveCda{
-		Creator:     signers[0].String(),
-		CdaId:       id,
-		SigningData: suite.GetSigningData(),
-	}
-	err := k.SetApproval(suite.Ctx, &msg)
-	suite.EqualError(err, "The CDA must have a status of pending to be approved: The CDA's status did not match the expected status.")
 }
